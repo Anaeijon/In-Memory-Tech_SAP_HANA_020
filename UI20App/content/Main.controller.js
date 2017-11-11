14c2142e5dcb108a6c4b93703ac137009bdd1753
Anaeijon <david.doering@knopf-und-wolle.de>
2017-11-11 14:13:50 +0100

    update

---
+++ UI20App/content/Main.controller.js
@@ -0,0 +1,19 @@
+// the controller for the view Main
+sap.ui.controller("content.Main", {
+	onInit: function() {
+		// this function is called when the view is instantiated.
+		// Used to modify the view before displaying
+	},
+	onExit: function() {
+		// this function is called when the view is destroyed.
+		// Used for clean-up activities
+	},
+	onAfterRendering: function() {
+		// this function is called when the view has been rendered.
+		// Used for post-rendering manipulation of the HTML code
+	},
+	onBeforeRendering: function() {
+		// this function is called before the view is re-rendered
+		// (not before first rendering)
+	}
+});
