66288558f0ed78d90eb60f13bee644f9cbed7cfe
Anaeijon <david.doering@knopf-und-wolle.de>
2017-11-11 14:51:31 +0100

    show pages

--- UI20App/content/Main.controller.js
+++ UI20App/content/Main.controller.js
@@ -15,5 +15,54 @@ sap.ui.controller("content.Main", {
 	onBeforeRendering: function () {
 		// this function is called before the view is re-rendered
 		// (not before first rendering)
+	},
+	// here we define what happens when a user clicks on a navigation item
+	itemSelected: function (oEvent) {
+		// first, we have to get the key of the selected navigation item
+		var itemKey = oEvent.getParameter("key");
+		// next, we need to get the shell to be able to manipulate it
+		var oShell = oEvent.oSource;
+		// next, we have to get the right page (content) for the key.
+		// For convenience reasons, this is done in a different function
+		var content = this.getContent(itemKey);
+		// last, we put the content that we retrieved to the content
+		// section of the shell. the first argument for the function is
+		// the content, the second one defines if we want to destroy old
+		// content (if present) before adding the new content
+		oShell.setContent(content, true);
+	},
+	// this functions gets the correct page and creates the view
+	getContent: function (key) {
+		var content = null;
+		// because the application will have several pages,
+		// we use the switch-case contruct to check the key
+		switch(key) {
+		case "overview":
+			// when the navigation item "overview" is selected,
+			// we return the respective view
+			content = sap.ui.view({
+				// location and name of the view
+				viewName: "content.Overview",
+				// type of the view, i.e. XML, JavaScript, JSON...
+				type: sap.ui.core.mvc.ViewType.XML
+			});
+			break;
+		case "salesOrders":
+			// when the navigation item "salesOrders" is selected,
+			// we return the respective view
+			content = sap.ui.view({
+				// location and name of the view
+				viewName: "content.SalesOrders",
+				// type of the view, i.e. XML, JavaScript, JSON...
+				type: sap.ui.core.mvc.ViewType.XML
+			});
+			break;
+		default:
+			// when we get an item that we do not know,
+			// we return nothing
+			content = null;
+			break;
+		}
+		return content;
 	}
 });
