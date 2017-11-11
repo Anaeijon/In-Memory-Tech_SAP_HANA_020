6a64ebdc1c96177a543ffb2e05a83d1672b3a5cd
Anaeijon <david.doering@knopf-und-wolle.de>
2017-11-11 15:10:16 +0100

    populated SalesOrders

--- UI20App/content/SalesOrders.controller.js
+++ UI20App/content/SalesOrders.controller.js
@@ -1,8 +1,13 @@
 sap.ui.controller("content.SalesOrders", {
 	// controller logic goes here
 	onInit: function () {
-		// this function is called when the view is instantiated.
-		// Used to modify the view before displaying
+		// first, we have to define the odata model
+		var dataModel = new sap.ui.model.odata.ODataModel(
+			"models/GBI.xsodata"
+		);
+		// after that, we can bind the odata model the
+		// SalesOrders view, so controls within the view can use them
+		this.getView().setModel(dataModel);
 	},
 	onExit: function () {
 		// this function is called when the view is destroyed.
@@ -15,5 +20,25 @@ sap.ui.controller("content.SalesOrders", {
 	onBeforeRendering: function () {
 		// this function is called before the view is re-rendered
 		// (not before first rendering)
+	},
+	// this function updates the details panel
+	updateDetails: function (oEvent) {
+		// first, we need to get the context of the selected
+		// row from the event
+		var rowContext = oEvent.getParameter("rowContext");
+		// next, we need the Input control that we want
+		// to manipulate
+		var customerNameInput = this.getView().byId("customerName");
+		// now we can set the Input to the value of a
+		// field of the selected row
+		customerNameInput.setValue(rowContext.getObject().CUSTOMER_NAME);
+		var customerCityInput = this.getView().byId("city");
+		customerCityInput.setValue(rowContext.getObject().CITY);
+		var customerCountryInput = this.getView().byId("country");
+		customerCountryInput.setValue(rowContext.getObject().SHORT_TEXT_COUNTRY);
+		var customerSalesOrgInput = this.getView().byId("salesOrg");
+		customerSalesOrgInput.setValue(
+			rowContext.getObject().SHORT_TEXT_SALESORG
+		);
 	}
 });
